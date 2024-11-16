4edcecee70613472e9275d0f350a55af165db273
SamrawitDawit <samrikdawit@gmail.com>
2024-11-16 14:31:56 +0300

    removed frontend folder and set up the server- the router and the main file

---
+++ Task1/backend/router/router.go
@@ -0,0 +1,25 @@
+package router
+
+import "github.com/gin-gonic/gin"
+
+func Router() {
+	router := gin.Default()
+	router.GET("/name", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"Name": "Samrawit Dawit",
+		})
+	})
+	router.GET("/hobby", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"1)": "Chatting with friends",
+			"2)": "Reading",
+			"3)": "Watching Movies",
+		})
+	})
+	router.GET("/dream", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"Dream": "To solve people's problem using technology",
+		})
+	})
+	router.Run(":8080")
+}
