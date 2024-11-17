1c5e26dba647502d9f3798ca55733cb8e3bc06d9
SamrawitDawit <samrikdawit@gmail.com>
2024-11-17 22:48:00 +0300

    added a '/' route and modified the name and dream to be returned as plain texts

--- Task1/api/main.go
+++ Task1/api/main.go
@@ -1,36 +1,42 @@
 package handler
 
 import (
-    "net/http"
+	"net/http"
 
-    "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-    gin.SetMode(gin.ReleaseMode)
-    router := gin.Default()
+	gin.SetMode(gin.ReleaseMode)
+	router := gin.Default()
 
-    router.GET("/name", func(c *gin.Context) {
-        c.JSON(200, gin.H{
-            "Name": "Samrawit Dawit",
-        })
-    })
-    router.GET("/hobby", func(c *gin.Context) {
-        c.JSON(200, gin.H{
-            "1)": "Chatting with friends",
-            "2)": "Reading",
-            "3)": "Watching Movies",
-        })
-    })
-    router.GET("/dream", func(c *gin.Context) {
-        c.JSON(200, gin.H{
-            "Dream": "To solve people's problem using technology",
-        })
-    })
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Welcome to my API...to get my name, hobby and dream, please use the following endpoints: /name, /hobby, /dream",
+		})
+	})
 
-    router.ServeHTTP(w, r)
+	router.GET("/name", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"Name": "Samrawit Dawit",
+		})
+	})
+	router.GET("/hobby", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"1)": "Chatting with friends",
+			"2)": "Reading",
+			"3)": "Watching Movies",
+		})
+	})
+	router.GET("/dream", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"Dream": "To solve people's problem using technology",
+		})
+	})
+
+	router.ServeHTTP(w, r)
 }
 
 func main() {
-    http.HandleFunc("/", Handler)
+	http.HandleFunc("/", Handler)
 }
