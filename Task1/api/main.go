6b4354ca269d3a3694a3b0653021328b8fe38d22
SamrawitDawit <samrikdawit@gmail.com>
2024-11-17 22:10:20 +0300

    Add main function to initialize the HTTP server

--- Task1/api/main.go
+++ Task1/api/main.go
@@ -1,33 +1,36 @@
-package router
+package main
 
 import (
-	"net/http"
+    "net/http"
 
-	"github.com/gin-gonic/gin"
+    "github.com/gin-gonic/gin"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	gin.SetMode(gin.ReleaseMode)
-	router := gin.Default()
-	router.GET("/name", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"Name": "Samrawit Dawit",
-		})
-	})
-	router.GET("/hobby", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"1)": "Chatting with friends",
-			"2)": "Reading",
-			"3)": "Watching Movies",
-		})
-	})
-	router.GET("/dream", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"Dream": "To solve people's problem using technology",
-		})
-	})
-	router.ServeHTTP(w, r)
+    gin.SetMode(gin.ReleaseMode)
+    router := gin.Default()
+
+    router.GET("/name", func(c *gin.Context) {
+        c.JSON(200, gin.H{
+            "Name": "Samrawit Dawit",
+        })
+    })
+    router.GET("/hobby", func(c *gin.Context) {
+        c.JSON(200, gin.H{
+            "1)": "Chatting with friends",
+            "2)": "Reading",
+            "3)": "Watching Movies",
+        })
+    })
+    router.GET("/dream", func(c *gin.Context) {
+        c.JSON(200, gin.H{
+            "Dream": "To solve people's problem using technology",
+        })
+    })
+
+    router.ServeHTTP(w, r)
 }
+
 func main() {
     http.HandleFunc("/", Handler)
 }
