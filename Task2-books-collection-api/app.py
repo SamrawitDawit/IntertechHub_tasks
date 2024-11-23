b617ed8d970bd9b27e911efe7b690706810bf15e
SamrawitDawit <samrikdawit@gmail.com>
2024-11-24 01:38:37 +0300

    changed local mongo db to mongo atlas and added vercel.json file

--- Task2-books-collection-api/app.py
+++ Task2-books-collection-api/app.py
@@ -5,4 +5,4 @@ app = Flask(__name__)
 initialize_routes(app)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
--- Task2-books-collection-api/app/database.py
+++ Task2-books-collection-api/app/database.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 import os
 
-client = MongoClient(os.getenv("MONGO_URI"))
+client = MongoClient("mongodb+srv://samrikdawit:it'sSamri22@intertechhub.a0g3q.mongodb.net/?retryWrites=true&w=majority&appName=Intertechhub")
 db = client["books_collection"]
 books = db.books
