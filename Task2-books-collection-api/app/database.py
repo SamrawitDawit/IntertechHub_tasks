4146b9152c28a94fe81cdd4cad5cb5d4f32cab83
SamrawitDawit <samrikdawit@gmail.com>
2024-11-24 03:03:58 +0300

    added the ssl option to the db  connection to enforce ssl

--- Task2-books-collection-api/app/database.py
+++ Task2-books-collection-api/app/database.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 import os
 
-client = MongoClient("mongodb+srv://samrikdawit:it'sSamri22@intertechhub.a0g3q.mongodb.net/?retryWrites=true&w=majority&appName=Intertechhub")
+client = MongoClient("mongodb+srv://samrikdawit:it'sSamri22@intertechhub.a0g3q.mongodb.net/?retryWrites=true&w=majority&appName=Intertechhub", tls=True)
 db = client["books_collection"]
 books = db.books
